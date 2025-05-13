e6ae4e7d86304a913bcb7f8358efb3bc1b680881
Roy Y Liao <yunzhi.liao@gmail.com>
2025-05-13 15:28:01 +1000

    secure mongo

--- src/app/lib/mongodb.js
+++ src/app/lib/mongodb.js
@@ -1,7 +1,6 @@
 import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
-// const uri = "mongodb+srv://roy:EdgeArk1@cluster0.xgke8hd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const options = {};
 
 let client;
